318a39cda516c96c4fd26bb96572ad9cc040c415
ElizavetaKr <elizovetakryukova@mail.ru>
2024-04-17 11:26:06 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParse() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void checkContainSymbol() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "Name";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("not contain the symbol");
+    }
+
+    @Test
+    void checkContainKey() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "=Name";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("not contain a key");
+    }
+
+    @Test
+    void checkContainValue() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "Name=";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("not contain a value");
+    }
+}
