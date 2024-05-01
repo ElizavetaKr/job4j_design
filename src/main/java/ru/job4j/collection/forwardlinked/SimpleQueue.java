17f39a9fdd724ca24c2a4913a896453bcc61daf2
ElizavetaKr <elizovetakryukova@mail.ru>
2024-05-01 15:15:16 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/forwardlinked/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/forwardlinked/SimpleQueue.java
@@ -6,32 +6,26 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> input = new SimpleStack<>();
     private final SimpleStack<T> output = new SimpleStack<>();
 
-    int size;
-    int out = 0;
-    int in = 0;
+    private int size;
+    private int out = 0;
+    private int in = 0;
 
     public T poll() {
         if (size == 0) {
             throw new NoSuchElementException("Queue is empty");
         }
-        if (in > 0) {
+        if (out == 0 && in > 0) {
             for (int i = size; i > 0; i--) {
                 output.push(input.pop());
             }
-            in = 0;
+            out = in;
         }
+        out--;
         size--;
-        out++;
         return output.pop();
     }
 
     public void push(T value) {
-        if (out > 0) {
-            for (int i = size; i > 0; i--) {
-                input.push(output.pop());
-            }
-            out = 0;
-        }
         input.push(value);
         size++;
         in++;
