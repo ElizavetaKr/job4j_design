74a2d3d8fb844dba1cf9ca0868ca114537ab2d18
ElizavetaKr <elizovetakryukova@mail.ru>
2024-05-29 21:10:10 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,55 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(n -> !n.isEmpty() && !n.startsWith("#"))
+                    .forEach(n -> {
+
+
+                        if (!n.matches(".+=.+")) {
+                            throw new IllegalArgumentException("Out of range");
+                        }
+                        String[] array = n.split("=", 2);
+                        values.put(array[0], array[1]);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithCommentAndEmptyLines() {
+        String path = "./data/pair_with_comment_and_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Elizaveta=");
+    }
+
+    @Test
+    void whenPatternNotComplete() {
+        String path = "./data/pattern_not_complete.properties";
+        Config config = new Config(path);
+        assertThrows(IllegalArgumentException.class, config::load);
+    }
+}
