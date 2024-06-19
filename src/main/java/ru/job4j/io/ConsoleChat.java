5192109f375620795c67c0ccc13c3aa5d67e6b29
ElizavetaKr <elizovetakryukova@mail.ru>
2024-06-19 22:51:39 +0300

    16. Кодировка [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,79 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        boolean answer = true;
+        String consoleAnswer;
+        String question = "";
+        while (!question.equals(OUT)) {
+            consoleAnswer = "Введите строку: ";
+            System.out.println(consoleAnswer);
+            saveLog(List.of(consoleAnswer, System.lineSeparator()));
+
+            Scanner console = new Scanner(System.in);
+            question = console.nextLine();
+            saveLog(List.of(question, System.lineSeparator()));
+
+            if (question.equals(STOP)) {
+                answer = false;
+            }
+
+            if (answer && !question.equals(OUT)) {
+                List<String> botAnswers = readPhrases();
+                int index = (int) (Math.random() * botAnswers.size());
+                consoleAnswer = botAnswers.get(index);
+                System.out.println(consoleAnswer);
+                saveLog(List.of(consoleAnswer, System.lineSeparator()));
+            }
+
+            if (question.equals(CONTINUE)) {
+                answer = true;
+            }
+        }
+
+    }
+
+    private List<String> readPhrases() {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(botAnswers, Charset.forName("UTF-8")))) {
+            result = reader.lines()
+                    .map(string -> string + System.lineSeparator())
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path, Charset.forName("UTF-8"), true))) {
+            log.forEach(writer::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat consoleChat = new ConsoleChat("C:/projects/job4j_design/data/path.txt",
+                "C:/projects/job4j_design/data/botAnswers.txt");
+        consoleChat.run();
+    }
+}
