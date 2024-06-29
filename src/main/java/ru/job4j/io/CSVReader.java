bdae31f839388b0b38a69a7a1ee43cfff688f414
ElizavetaKr <elizovetakryukova@mail.ru>
2024-06-29 09:34:50 +0300

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -8,33 +8,43 @@ import java.util.StringJoiner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        final String delimiter = argsName.get("delimiter");
         File file = new File(argsName.get("path"));
         try (var scanner = new Scanner(file)
                 .useDelimiter(",");
              BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(argsName.get("out")))) {
 
-            List<String> param = List.of(scanner.nextLine().split(argsName.get("delimiter")));
+            List<String> param = List.of(scanner.nextLine().split(delimiter));
             List<String> filter = List.of(argsName.get("filter").split(","));
             List<Integer> index = new ArrayList<>();
 
-            StringJoiner joiner = new StringJoiner(argsName.get("delimiter"));
+            StringJoiner joiner = new StringJoiner(delimiter);
+
             for (String str : filter) {
                 if (param.contains(str)) {
                     index.add(param.indexOf(str));
                     joiner.add(str);
                 }
             }
-            output.write((joiner + System.lineSeparator()).getBytes());
+            if ("stdout".equals(argsName.get("out"))) {
+                System.out.println(joiner);
+            } else {
+                output.write((joiner + System.lineSeparator()).getBytes());
+            }
+
 
             while (scanner.hasNext()) {
-                joiner = new StringJoiner(argsName.get("delimiter"));
-                param = List.of(scanner.nextLine().split(argsName.get("delimiter")));
+                joiner = new StringJoiner(delimiter);
+                param = List.of(scanner.nextLine().split(delimiter));
 
                 for (Integer i : index) {
                     joiner.add(param.get(i));
                 }
-
-                output.write((joiner + System.lineSeparator()).getBytes());
+                if ("stdout".equals(argsName.get("out"))) {
+                    System.out.println(joiner);
+                } else {
+                    output.write((joiner + System.lineSeparator()).getBytes());
+                }
             }
         }
     }
@@ -47,9 +57,6 @@ public class CSVReader {
         File file = new File(param.get("path"));
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
-        }
-        if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         if (!param.get("path").endsWith(".csv")) {
             throw new IllegalArgumentException("The extension of \"path\" is not set correctly");
